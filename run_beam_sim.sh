b6c4143f2d13b091c47158ec80c93a05ee3dafe1
Anna Zuckerman <anna@nwlink.com>
2025-09-16 17:36:24 -0600

    Update arguments

--- run_beam_sim.sh
+++ run_beam_sim.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/bash
 
-# change according to your computing resources, of course
-#SBATCH --account=ucb338_asc3
-#SBATCH --ntasks=96
-#SBATCH --ntasks-per-node=32
-#SBATCH --nodes=3
-#SBATCH --time=6:00:00
+#SBATCH --account=your_account
+#SBATCH --ntasks=64
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
 #SBATCH --job-name=beam_sim_run
-#SBATCH --partition=amilan
-#SBATCH --output=beam_sim_run.%j.out
-#SBATCH --constraint=ib 
+#SBATCH --partition=your_partition
+#SBATCH --output=beam_sim_run.%A_%a.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=your_email
+#SBATCH --qos=normal
 #SBATCH --array=0-7
 
-# activate conda environment
-source activate base
-conda activate beam_sim_env
+# activate conda environment (will vary depending on where you're running this of course)
+module load anaconda
+conda activate pymc_env
+
+# enable buffer
+export PYTHONUNBUFFERED=TRUE
 
 # Start a new simulation.
 # Note that the task ID array above must match the length of the energy list provided. This will run the simulation once for each array value,
 # taking the value of the energy array at that index. 
-# Implemented atmosphere types: 'T900_g5.0', 'T1400_g4.0', 'T900_g4.0', 'T1400_g5.0', 'T2000_g5.0', 'T482_g4.7', 'Jupiter'
-atm_type=Jupiter
-energies=(0.1 0.5 1 5 10 50 100 500)
-logfile=beam_sim_run.$SLURM_JOB_ID.out
-sonora_filepath=../../atm_data # must have saved atmospheric density profile data here
-python ../../beam_simulation_clean.py --Ne0 1000 --Nsteps 20000 --e0 ${energies[$SLURM_ARRAY_TASK_ID]} --atm_type $atm_type --sonora_filepath $sonora_filepath --d --s --logfile $logfile --cos_theta 1
+atm_type=T2000_g5.0 # See implemented types in beam_simulation_clean.py
+energies=(500 100 50 10 5 1 0.5 0.1)
+logfile=beam_sim_run.${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID}.out
+sonora_filepath=../../Sonora_data # atmospheric profile output from Picaso/Sonora model, available upon request
+python ../../beam_simulation_clean.py --Ne0 1000 --e0 ${energies[$SLURM_ARRAY_TASK_ID]} --atm_type $atm_type --sonora_filepath $sonora_filepath --d --s --min --logfile $logfile --cos_theta 1
+
+
+
+
 
 
 
